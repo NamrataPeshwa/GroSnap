d6564fed7196b342417a44c8f07eb97040209247
Namrata Peshwa <namratapeshwa04@gmail.com>
2025-06-14 23:01:54 +0530

    Update config.py
    removed the personal key info

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ class Config:
     SECRET_KEY = 'your-secret-key'
 
     # MySQL configuration
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:Helloworld123@localhost:3306/shopkeeper'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:name@localhost:number/shopkeeper'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
